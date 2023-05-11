292ab0b8bab9fd9f91359b0839a4008f272150c5
Aditya Rai <kaditya1298@gmail.com>
2023-05-12 01:13:20 +0530

    simple cdk hit counter.

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,19 +1,28 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigw  from 'aws-cdk-lib/aws-apigateway';
+import {HitCounterStack} from  './hit_counter';
+import {TableViewer} from 'cdk-dynamo-table-viewer'
 
 export class HelloCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+  const hello_lambda = new lambda.Function(this, 'HelloHandler', 
+  {
+    runtime: lambda.Runtime.NODEJS_16_X,
+    code: lambda.Code.fromAsset('lambda'),
+    handler: 'hello.handler',
+  });
 
-    const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
+  const hit_counter = new HitCounterStack(this, 'HelloHitCounter', {
+    downstream: hello_lambda
+  });
 
-    const topic = new sns.Topic(this, 'HelloCdkTopic');
+  const apigateway = new apigw.LambdaRestApi(this, 'Endpoint', {
+    handler: hit_counter.handler
+  });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
-  }
+
+}
 }
